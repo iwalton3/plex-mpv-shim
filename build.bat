9d6dd0f763c5a7ed0915e1b805d53cea3c870716
Ian Walton <ian@iwalton.com>
2021-04-21 18:59:24 -0400

    Add hidpi and switch to installer.

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 @echo off
 git pull
 rd /s /q __pycache__ dist build
-pyinstaller -wF --add-binary "mpv-1.dll;." --add-binary "plex_mpv_shim\systray.png;." --add-data "plex_mpv_shim\default_shader_pack;plex_mpv_shim\default_shader_pack" --hidden-import pystray._win32 --icon media.ico run.py
+pyinstaller -w --add-binary "mpv-1.dll;." --add-binary "plex_mpv_shim\systray.png;." --add-data "plex_mpv_shim\default_shader_pack;plex_mpv_shim\default_shader_pack" --hidden-import pystray._win32 --icon media.ico run.py
+if %errorlevel% neq 0 exit /b %errorlevel%
+del dist\run\run.exe.manifest
+copy hidpi.manifest dist\run\run.exe.manifest
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "Plex MPV Shim.iss"
+if %errorlevel% neq 0 exit /b %errorlevel%
