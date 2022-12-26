05e37ca4905f2810f12ebf1d2292b6802a1d8447
Ian Walton <ian@iwalton.com>
2022-12-26 14:12:22 -0500

    Do not use idle event to fix for python-mpv 1.0.1. (#72)

--- plex_mpv_shim/player.py
+++ plex_mpv_shim/player.py
@@ -236,9 +236,9 @@ class PlayerManager(object):
                 self.put_task(self.finished_callback, has_lock)
 
         # Fires at the end.
-        @self._player.event_callback('idle')
-        def handle_end_idle(event):
-            if self._media_item:
+        @self._player.property_observer("playback-abort")
+        def handle_end_idle(_name, value):
+            if self._media_item and value:
                 has_lock = self._finished_lock.acquire(False)
                 self.put_task(self.finished_callback, has_lock)
 
