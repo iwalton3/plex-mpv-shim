2cd712453c8b3d43c6901bdaa9d93ef84999f7de
Ian Walton <ian@iwalton.com>
2023-02-14 19:41:43 -0500

    Upgrade MPV version to 20230205 e439ddc

--- build-debug.bat
+++ build-debug.bat
@@ -1,4 +1,4 @@
 @echo off
 git pull
 rd /s /q __pycache__ dist build
-pyinstaller -cF --add-binary "mpv-1.dll;." --add-binary "plex_mpv_shim\systray.png;." --icon media.ico run.py --hidden-import pystray._win32
+pyinstaller -cF --add-binary "mpv-2.dll;." --add-binary "plex_mpv_shim\systray.png;." --icon media.ico run.py --hidden-import pystray._win32
--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 git pull
 rd /s /q __pycache__ dist build
-pyinstaller -w --add-binary "mpv-1.dll;." --add-binary "plex_mpv_shim\systray.png;." --add-data "plex_mpv_shim\default_shader_pack;plex_mpv_shim\default_shader_pack" --hidden-import pystray._win32 --icon media.ico run.py
+pyinstaller -w --add-binary "mpv-2.dll;." --add-binary "plex_mpv_shim\systray.png;." --add-data "plex_mpv_shim\default_shader_pack;plex_mpv_shim\default_shader_pack" --hidden-import pystray._win32 --icon media.ico run.py
 if %errorlevel% neq 0 exit /b %errorlevel%
 del dist\run\run.exe.manifest
 copy hidpi.manifest dist\run\run.exe.manifest
